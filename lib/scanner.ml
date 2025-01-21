06aec32ec759ba13ab32a1014377ca312cbeac89
Ivan Hrekov <iandrc9@gmail.com>
2025-01-21 19:25:28 +0100

    experimenting with emitting tokens

--- lib/scanner.ml
+++ lib/scanner.ml
@@ -8,12 +8,50 @@ type state = {
   mutable current : int;
   line : int
 }
+type token = { t: Token.t; text : string; line : int }
 
 let default source = { source; start = 0; current = 0; line = 1}
-
 let is_at_end state = state.current >= String.length state.source
 
-let scan_token () = prerr_endline "scan token"
+let add_token state _ _ =
+  print_endline ("source:" ^ state.source ^ "start: " ^ string_of_int state.start ^ "current: " ^ string_of_int state.current)
+  (* let text = String.sub state.source state.start state.current in
+  print_endline text *)
+  (* let token = { t; text; line : state.line } in
+  Dynarray.add_last tokens token *)
+
+let emit_token tokens t = add_token tokens t
+
+let advance state =
+  print_endline ("source:" ^ state.source ^ "start: " ^ string_of_int state.start ^ "current: " ^ string_of_int state.current);
+
+  if not (is_at_end state)
+  then begin
+    state.current <- state.current + 1;
+    state.source.[state.current]
+  end
+  else ' '
+
+
+let add_token tokens token =
+  Dynarray.add_last tokens token
+
+(* How to make "default" case redundant? *)
+let scan_token tokens state =
+  let c = advance state in
+  print_char c;
+  match c with
+    | '(' -> emit_token state tokens Token.Right_paren
+    (* | ')' -> add_token tokens Token.Right_paren
+    | '{' -> add_token tokens Token.Left_brace
+    | '}' -> add_token tokens Token.Right_brace
+    | ',' -> add_token tokens Token.Comma
+    | '.' -> add_token tokens Token.Dot
+    | '-' -> add_token tokens Token.Minus
+    | '+' -> add_token tokens Token.Plus
+    | ';' -> add_token tokens Token.Semicolon
+    | '*' -> add_token tokens Token.Star *)
+    | _ -> print_endline "lol"
 
 (* Should return the list of tokens *)
 (* HOW TO PARSE PROPERLY *)
@@ -22,7 +60,7 @@ let scan_tokens source =
   let tokens = Dynarray.create () in
   while not (is_at_end s) do
     s.start <- s.current;
-    scan_token ();
+    scan_token tokens s;
   done;
   Dynarray.add_last tokens Token.Eof;
   tokens
